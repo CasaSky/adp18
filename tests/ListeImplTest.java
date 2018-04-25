9329b0991c963939578205523a084368c793f2fc
Michel Brüger <michel.brueger@haw-hamburg.de>
2018-04-25 12:13:43 +0200

    TestFile angelegt

---
+++ tests/ListeImplTest.java
@@ -0,0 +1,49 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by michelbrueger on 25.04.18.
+ */
+public class ListeImplTest {
+    @Test
+    public void size() throws Exception {
+    }
+
+    @Test
+    public void insert() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void clear() throws Exception {
+    }
+
+    @Test
+    public void concat() throws Exception {
+    }
+
+    @Test
+    public void sublist() throws Exception {
+    }
+
+    @Test
+    public void toString() throws Exception {
+    }
+
+    @Test
+    public void getSize() throws Exception {
+    }
+
+    @Test
+    public void getArrayListe() throws Exception {
+    }
+
+}
