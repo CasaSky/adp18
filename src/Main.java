083b7181c06e2e0c6c55418689182c35c3bbee51
Michel Brüger <michel.brueger@haw-hamburg.de>
2018-05-09 15:38:52 +0200

    RandomListGenerator...   bin ich zu doof für

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        Liste2ChainImpl<Integer> liste0 = new Liste2ChainImpl<>();
+//        Liste2ChainImpl<Integer> liste0 = new Liste2ChainImpl<>();
 //        liste0.insert(10,0);
 //        System.out.println(liste0.get(0));
 //        liste0.insert(100,0);
@@ -19,8 +21,8 @@ public class Main {
 //        System.out.println(liste0.get(4));
 //        System.out.println(liste0.get(2));
 //        System.out.println(liste0.get(2));
-        Liste2ChainImpl<Integer> liste1 = new Liste2ChainImpl<>();
-        liste1.insert(10, 0);
+//        Liste2ChainImpl<Integer> liste1 = new Liste2ChainImpl<>();
+//        liste1.insert(10, 0);
 //        System.out.println("---------------------------------------------");
 //        System.out.println(liste0.getClass());
 //        System.out.println("-----------------------------------------------");
@@ -31,9 +33,22 @@ public class Main {
 //        System.out.println(liste1.getSize());
 //        System.out.println(liste0.getSize());
 //        System.out.println(liste0.equals(liste1));
-        System.out.println(liste0.concat(liste1).equals(liste1));
-        System.out.println(liste0.concat(liste1));
-        System.out.println(liste1);
+//        System.out.println(liste0.concat(liste1).equals(liste1));
+//        System.out.println(liste0.concat(liste1));
+//        System.out.println(liste1);
+//        Random random = new Random();
+//        int randomNumber = random.ints(1,0,2).findFirst().getAsInt();
+//        System.out.println(randomNumber);
 
+        RandomListGenerator rlg = new RandomListGenerator();
+
+        ListeInterface testListe = rlg.generateRandomList(3,0,4,0,0);
+        System.out.println(testListe);
+        System.out.println(rlg.getRandomNumbersArray());
+        System.out.println(rlg.getSizeOfRandomNumbersArray());
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println(rlg.getRandomNumbersArray()[0]);
+        System.out.println(rlg.getRandomNumbersArray()[1]);
+        System.out.println(rlg.getRandomNumbersArray()[2]);
     }
 }
--- src/RandomListGenerator.java
+++ src/RandomListGenerator.java
@@ -1,18 +1,59 @@
 import java.util.Random;
 
 public class RandomListGenerator {
+    private static int[] randomNumbersArray;
 
     public RandomListGenerator(){
     }
 
-    public static ListeInterface generateRandomList(int listSize, int numbersFromInclusive, int numbersUpToExclusive){
+    public static ListeInterface generateRandomList(int listSize, int numbersFromInclusive, int numbersUpToExclusive, int insertAt, int listType){
+        Random random1 = new Random();
+        randomNumbersArray = random1.ints(listSize, numbersFromInclusive, numbersUpToExclusive).toArray();
+        ListeInterface resultList;
 
+        switch (listType) {
+            case 0:  resultList = new ListeImpl(40000);
+                    break;
+            case 1:  resultList = new Liste2ChainImpl();
+                    break;
+            default: throw new IllegalArgumentException("Please insert correct value for \"listType\", 0 for ListeImpl, 1 for Liste2ChainImpl");
+        }
+
+//        for (int elem : randomNumbersArray){
+//        for(int i=0; i<randomNumbersArray.length; i++){
+            switch (insertAt) {
+                case 0: for (int elem : randomNumbersArray){
+                            resultList.insert(elem,0);
+                }
+                        break;
+                case 1: for (int elem : randomNumbersArray){
+                            resultList.insert(elem, computeRandomPosition(resultList));
+                }
+                        break;
+                case 2: for (int elem : randomNumbersArray){
+                            resultList.insert(elem, resultList.getSize());
+                }
+                        break;
+                default: throw new IllegalArgumentException("Please insert correct value for \"insertAt\", 0 for insertion at beginning of list, 1 for insertion at random position, 2 for insertion at end of list ");
+//                        break;
+            }
+//        }
+        return resultList;
     }
 
-    Random random1 = new Random();
-    int[] randomNumbersArray = random1.ints(40000, 0, 11).toArray();
-    int randomNumber
+    private static int computeRandomPosition(ListeInterface list) {
+        Random random2 = new Random();
+        int randomPosition = random2.ints(1,0,list.getSize()+1).findFirst().getAsInt();
+        return randomPosition;
+    }
 
+    public int[] getRandomNumbersArray(){
+        return randomNumbersArray;
+    }
+
+    public int getSizeOfRandomNumbersArray(){
+        return randomNumbersArray.length;
+    }
 
 
 }
