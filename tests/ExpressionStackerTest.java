20000d6745ea9172f967c2f3b8fa301fd7ade084
Michel Brüger <michel.brueger@haw-hamburg.de>
2018-05-12 17:44:50 +0200

    Klasse Token erstellt, denke fertig, keine Tests (auch eher nicht nötig) Klasse ExpressionStacker erstellt,    computePartialExpression() angetestet, andere Tests fehlen noch Klasse Stack von Prof integriert, keine Tests

--- tests/ExpressionStackerTest.java
+++ tests/ExpressionStackerTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -6,8 +7,32 @@ import static org.junit.Assert.*;
  * Created by michelbrueger on 12.05.18.
  */
 public class ExpressionStackerTest {
+    Token operand1;
+    Token operand2;
+    Token operatorPlus;
+    Token operatorMinus;
+    Token operatorAsterisk;
+    Token operatorSlash;
+    Token dummy;
+
+
+    @Before
+    public void setup() throws Exception {
+        operand1 = new Token(10);
+        operand2 = new Token(20);
+        operatorPlus = new Token('+');
+        operatorMinus = new Token('-');
+        operatorAsterisk = new Token('*');
+        operatorSlash = new Token('/');
+        dummy = new Token('(');
+    }
+
     @Test
     public void computePartialExpression() throws Exception {
+        assertEquals(30, ExpressionStacker.computePartialExpression(operand1.getOperand(), operand2.getOperand(), operatorPlus.getOperator()));
+        assertEquals(-10, ExpressionStacker.computePartialExpression(operand1.getOperand(),operand2.getOperand(), operatorMinus.getOperator()));
+        assertEquals(200, ExpressionStacker.computePartialExpression(operand1.getOperand(), operand2.getOperand(), operatorAsterisk.getOperator()));
+        assertEquals(0, ExpressionStacker.computePartialExpression(operand1.getOperand(), operand2.getOperand(), operatorSlash.getOperator()));
     }
 
 }
